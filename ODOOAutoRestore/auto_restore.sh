0225dacdb37b84d12edf6448c9904fe6cad83f85
JenerationZ25 <98948462+JenerationZ25@users.noreply.github.com>
2023-02-01 14:35:18 +1100

    Update auto_restore.sh

--- ODOOAutoRestore/auto_restore.sh
+++ ODOOAutoRestore/auto_restore.sh
@@ -3,13 +3,13 @@ echo "starting..."
 
 cd C:/baraja/odoo_sh/
 
-server="4577487@barajait-odoo15sh.odoo.com"
-#file="2022-04-19_054527-barajait-odoo15sh-staging-4591955_manual.sql.gz"
-file="barajait-odoo15sh-proddb-4577487_daily.sql.gz"
-#sqlfile="2022-04-19_054527-barajait-odoo15sh-staging-4591955_manual.sql"
-sqlfile="barajait-odoo15sh-proddb-4577487_daily.sql"
-dir="/home/odoo/backup.daily/barajait-odoo15sh-proddb-4577487_daily.sql.gz"
-#dir="/home/odoo/backup.daily/barajait-odoo15sh-staging-4883013_daily.sql.gz"
+server="----------------.odoo.com"
+#file="---------.sql.gz"
+file="----------------.sql.gz"
+#sqlfile="----------------.sql"
+sqlfile="----------------.sql"
+dir="/home/----------------.sql.gz"
+#dir="/home/----------------.sql.gz"
 
 echo "downloading backup..."
 scp -i "baraja_nopass" $server:$dir .
@@ -17,21 +17,21 @@ scp -i "baraja_nopass" $server:$dir .
 
 echo "unzip backup..."
 #echo $file
-#"C:\Program Files\7-Zip\7z" e barajait-odoo15sh-proddb-4577487_daily.sql.gz -o ./
+#"C:\Program Files\7-Zip\7z" e ----------------.sql.gz -o ./
 
 "C:\Program Files\7-Zip\7z" e $file -o.\
 
 echo "restoring backup..."
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -d baraja -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'baraja_old' AND pid <> pg_backend_pid();"
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -c "DROP DATABASE baraja_old;"
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -d baraja -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'baraja_new' AND pid <> pg_backend_pid();"
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -c "DROP DATABASE baraja_new;"
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -c "CREATE DATABASE baraja_new"
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -d baraja_new -f ./$sqlfile
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -d baraja -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'baraja' AND pid <> pg_backend_pid();"
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -d baraja_new -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'baraja_new' AND pid <> pg_backend_pid();"
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -d baraja_new -c "ALTER DATABASE baraja RENAME TO baraja_old;"
-"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h odoosh-bar-aws.csqvxaloczsm.ap-southeast-2.rds.amazonaws.com  -U baraja -d baraja_old -c "ALTER DATABASE baraja_new RENAME TO baraja;"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -d ---- -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '----_old' AND pid <> pg_backend_pid();"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -c "DROP DATABASE ----_old;"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -d ---- -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '----_new' AND pid <> pg_backend_pid();"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -c "DROP DATABASE ----_new;"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -c "CREATE DATABASE ----_new"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -d ----_new -f ./$sqlfile
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -d ---- -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '----' AND pid <> pg_backend_pid();"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -d ----_new -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '----_new' AND pid <> pg_backend_pid();"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ---------------------------------------------------------------- -U ---- -d ----_new -c "ALTER DATABASE ---- RENAME TO ----_old;"
+"C:\Program Files\PostgreSQL\14\bin\psql" -p 5432 -h ----------------------------------------------------------------  -U ---- -d ----_old -c "ALTER DATABASE ----_new RENAME TO ----;"
 
 rm ./$sqlfile
 
